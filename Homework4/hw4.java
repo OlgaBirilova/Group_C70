3d776d08750d0bf9d4ac1b3e1fa240d0b8dce3ac
OlgaBirilova <olga.birilova97@gmail.com>
2023-09-08 14:04:29 +0300

    Commit homework4

--- Homework4/hw4.java
+++ Homework4/hw4.java
@@ -1,6 +1,5 @@
 package Homework4;
 import java.util.Arrays;
-import java.util.Scanner;
 public class hw4 {
 //    Task 1:
 //    Пройти по массиву, вывести все элементы в прямом и обратном порядке.
@@ -116,5 +115,35 @@ public class hw4 {
                 System.out.println("Массив не является возрастающей последовательностью.");
             }
 //        Task *:
+//        Имеется массив из неотрицательных чисел(любой). Представьте что массив
+//        представляет целое число (Например массив {1,2,3} -> 123, {9,9,9} -> 999). Задача
+//        добавить единицу к этому “числу” и на выходе получить исправленный массив. Массив не
+//        содержит нуля в начале, кроме самого числа 0.
+//        Пример:
+//        Input: [1,4,0,5,6,3]
+//        Output: [1,4,0,5,6,4]
+//        Input: [9,9,9]
+//        Output: [1,0,0,0]
+        System.out.println("///Task *:///");
+        int[] array = new int [4];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = (int) (Math.random() * 10);
+        }
+        int result = 0;
+        for (int i =array.length - 1 , n = 0; i >= 0; --i, n++) {
+            int pos = (int)Math.pow(10, i);
+            result += array[n] * pos;
+        }
+        System.out.println(Arrays.toString(array));
+//        System.out.println(result);
+        result++;
+//        System.out.println(result);
+        int[] newArray = new int[String.valueOf(result).length()];
+//        System.out.println(String.valueOf(result).length());
+        for (int i = newArray.length -1; i >= 0; i--) {
+            newArray[i] = result % 10;
+            result /= 10;
+        }
+        System.out.println(Arrays.toString(newArray));
     }
 }

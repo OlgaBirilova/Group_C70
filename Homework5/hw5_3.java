3662e8ec897643b862db72a209a9154f8de9adfd
OlgaBirilova <olga.birilova97@gmail.com>
2023-09-11 19:05:46 +0300

    Commit homework5

--- Homework5/hw5_3.java
+++ Homework5/hw5_3.java
@@ -12,17 +12,16 @@ public class hw5_3 {
         int[][] task5_3 = new int[n][m];
         int value = 0;
         for (int i = 0; i < n; i++) {
-            if (i % 2 == 0) {
-                for (int j = 0; j < m; j++) {
+            if (i % 2 != 0) {
+                for (int j = m - 1; j >= 0; j--) {
                     task5_3[i][j] = value++;
                 }
             } else {
-                for (int j = m - 1; j >= 0; j--) {
+                for (int j = 0; j < m; j++) {
                     task5_3[i][j] = value++;
                 }
             }
         }
-
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
                 System.out.printf("%3d", task5_3[i][j]);

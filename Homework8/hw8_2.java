652a89f5d0cd95132482f36c24d829d50e07cc10
OlgaBirilova <olga.birilova97@gmail.com>
2023-09-21 16:12:49 +0300

    Commit homework8

--- Homework8/hw8_2.java
+++ Homework8/hw8_2.java
@@ -1,5 +1,19 @@
 package Homework8;
 
 public class hw8_2 {
+    static class Apple {
+        private String color = "green";
 
+        public String getColor() {
+            return color;
+        }
+    }
+
+    public class Main {
+        public static void main(String[] args) {
+            Apple apple = new Apple();
+            apple.color = "blue";
+            System.out.println(apple.getColor());
+        }
+    }
 }

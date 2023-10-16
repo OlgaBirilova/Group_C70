5a136b810522bfc427433912c854ba0826b494aa
OlgaBirilova <olga.birilova97@gmail.com>
2023-10-16 19:34:43 +0300

    Commit classwork15

---
+++ Homework15/practice/User.java
@@ -0,0 +1,20 @@
+package Homework15.practice;
+import java.util.Optional;
+public class User {
+    private String name;
+
+    public User(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public static void main(String[] args) {
+        User user = null;
+        Optional<User> optionalUser = Optional.ofNullable(user);
+        String userName = optionalUser.map(User::getName).orElse("DEFAULT");
+        System.out.println(userName);
+    }
+}

b96cf533f3a13c89a4a567c332ac866984538e44
OlgaBirilova <olga.birilova97@gmail.com>
2023-10-16 21:03:55 +0300

    Commit classwork14

---
+++ Homework14/practice/task4.java
@@ -0,0 +1,20 @@
+package Homework14.practice;
+import java.util.function.BiFunction;
+public class task4 {
+    public static void main(String[] args) {
+        BiFunction<Integer, Integer, Integer> logic = (a, b) -> {
+            if (a < b) {
+                return a;
+            } else if (a > b) {
+                return b;
+            } else {
+                return 0;
+            }
+        };
+        int result1 = logic.apply(3, 5);
+        int result2 = logic.apply(7, 2);
+        int result3 = logic.apply(4, 4);
+        System.out.println(result2);
+        System.out.println(result3);
+    }
+}
